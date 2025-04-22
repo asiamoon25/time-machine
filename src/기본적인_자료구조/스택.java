67cdb8e40c0cc49d1fce2c3a0c489405ebf0a277
yeonsang <wjddustkd45@gmail.com>
2025-04-22 18:47:22 +0900

    회사에서 몰래 하는 중

--- src/기본적인_자료구조/스택.java
+++ src/기본적인_자료구조/스택.java
@@ -1,72 +1,58 @@
 package 기본적인_자료구조;
-
+//https://www.acmicpc.net/problem/10828
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class 스택 {
+
     public static void main(String[] args) {
+
         Stack<Integer> stack = new Stack<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = 14;
+        try{
+            String input = br.readLine();
 
-        String command1 = "push 1";
-        String command2 = "push 2";
-        String command3 = "top";
-        String command4 = "size";
-        String command5 = "empty";
-        String command6 = "pop";
-        String command7 = "pop";
-        String command8 = "pop";
-        String command9 = "size";
-        String command10 = "empty";
-        String command11 = "pop";
-        String command12 = "push 3";
-        String command13 = "empty";
-        String command14 = "top";
-        doCommand(stack, command1);
-        doCommand(stack, command2);
-        doCommand(stack, command3);
-        doCommand(stack, command4);
-        doCommand(stack, command5);
-        doCommand(stack, command6);
-        doCommand(stack, command7);
-        doCommand(stack, command8);
-        doCommand(stack, command9);
-        doCommand(stack, command10);
-        doCommand(stack, command11);
-        doCommand(stack, command12);
-        doCommand(stack, command13);
-        doCommand(stack, command14);
+            Integer n = Integer.parseInt(input);
+            List<Integer> result = new ArrayList<>();
+            for(int i = 0; i < n; i++) {
+                String command = br.readLine();
 
-
-    }
-
-    public static void doCommand(Stack<Integer> stack, String commandLine) {
-
-        if(commandLine.startsWith("push")) {
-            String number = commandLine.split(" ")[1];
-            stack.push(Integer.parseInt(number));
-        }else {
-            if("pop".equals(commandLine)){
-
-                if(stack.isEmpty()){
-                    System.out.println(-1);
-                } else {
-                    System.out.println(stack.pop());
-                }
-            } else if("size".equals(commandLine)) {
-                System.out.println(stack.size());
-            } else if("empty".equals(commandLine)) {
-                System.out.println(stack.isEmpty() ? 1 : 0);
-            } else if("top".equals(commandLine)){
-                if(stack.isEmpty()){
-                    System.out.println(-1);
-                } else {
-                    System.out.println(stack.peek());
+                if(command.startsWith("push")) {
+                    String number = command.split(" ")[1];
+                    stack.push(Integer.parseInt(number));
+                }else {
+                    if("pop".equals(command)){
+                        if(stack.isEmpty()){
+                            result.add(-1);
+                        } else {
+                            result.add(stack.pop());
+                        }
+                    } else if("size".equals(command)) {
+                        result.add(stack.size());
+                    } else if("empty".equals(command)) {
+                        result.add(stack.isEmpty() ? 1 : 0);
+                    } else if("top".equals(command)){
+                        if(stack.isEmpty()){
+                            result.add(-1);
+                        } else {
+                            result.add(stack.peek());
+                        }
+                    }
                 }
             }
+
+
+            for(int i = 0; i < result.size(); i++) {
+                System.out.println(result.get(i));
+            }
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
-
 }
 /*
 정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
